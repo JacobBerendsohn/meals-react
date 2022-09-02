84705b261dee5a3c8b8da0d4be5d1c44846bbe88
Jacob Berendsohn <Jacob.Berendsohn1@marist.edu>
2022-09-02 10:33:30 -0400

    added and imported routes from router dom

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import Favorites from './components/Favorites';
 import Meals from './components/Meals';
 import Modal from './components/Modal';
 import Search from './components/Search';
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        
+      </Routes>
       {/* <Search /> */}
       {/* <Favorites /> */}
       <Meals />
